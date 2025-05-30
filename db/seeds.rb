d5ef9d7f9746b171e2ca7296be42b1aad1d6e7f8
Noah Fonseca <nfonseca@rrc.ca>
2025-05-30 12:12:57 -0500

    Added seed to database with Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+# Create some products with Faker
+676.times do
+  Product.find_or_create_by!(
+    title: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 10.0..100.0),
+    stock_quantity: Faker::Number.between(from: 1, to: 100)
+  )
+end
