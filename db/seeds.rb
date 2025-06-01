0e4f965320cc65fe78244de058f6423f74f83317
Noah Fonseca <nfonseca@rrc.ca>
2025-06-01 10:50:12 -0500

    Added Category table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require "faker"
+
+# require "faker"
 # Create some products with Faker
-676.times do
-  Product.find_or_create_by!(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price(range: 10.0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+# 676.times do
+#   Product.find_or_create_by!(
+#     title: Faker::Commerce.product_name,
+#     price: Faker::Commerce.price(range: 10.0..100.0),
+#     stock_quantity: Faker::Number.between(from: 1, to: 100)
+#   )
+# end
+require "csv"
+
+Product.destroy_all
+Category.destroy_all
+
+CSV.foreach("db/products.csv", headers: true) do |row|
+  Product.create_or_find_by!(
+    title: row["name"],
+    price: row["price"].to_f,
+    stock_quantity: row["stock_quantity"].to_i,
+    category: Category.find_or_create_by!(name: row["category"])
   )
 end
